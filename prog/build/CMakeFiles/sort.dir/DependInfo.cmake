6836dbc52d77c829891d572d7e55c9acf26cf875
Igor Dimitrov <igor-dimi@protonmail.com>
2024-06-27 15:17:20 +0200

    programming problem

---
+++ prog/build/CMakeFiles/sort.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/igord/Documents/uni/ss24/alda/sol/prog/sort.cpp" "CMakeFiles/sort.dir/sort.cpp.o" "gcc" "CMakeFiles/sort.dir/sort.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
